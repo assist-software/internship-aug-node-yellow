71a2f2f7aefdcce6caa1fd92b7f487ccd443fbd7
Madalina <madalina.agheorghiesei@gmail.com>
2020-08-11 16:48:38 +0300

    completed data model ClubInvite

--- app/models/club-invite.model.js
+++ app/models/club-invite.model.js
@@ -3,6 +3,14 @@ module.exports = (sequilize, Sequilize) => {
         email: {
             type: Sequilize.STRING,
             allowNull: false
+        },
+        club_id: {
+            type: Sequilize.INTEGER,
+            references: {
+                model: Club,
+                key: 'id'
+            },
+            allowNull: false
         }
     });
     return ClubInvite;
