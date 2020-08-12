c94007490d1c6c9f427b10ff516df6b4e5de5e2c
Madalina <madalina.agheorghiesei@gmail.com>
2020-08-12 17:57:24 +0300

    club-member controller and api's

---
+++ app/controllers/club-member.controller.js
@@ -0,0 +1,70 @@
+const db = require("../models");
+const ClubMember = db.clubMember;
+//const Op = db.Op;
+
+exports.create = (req, res) => {
+  //Validate request
+  if(!req.body.user_id || !req.body.club_id) {
+      res.status(404).send({
+          message: "Content cannot be empty!"
+      });
+      return;
+  }
+   //Create ClubMember
+   const clubMemeber = {
+       user_id: req.body.user_id,
+       club_id: req.body.club_id
+   };
+
+   //Save clubMember in the db
+   ClubMember.create(clubMemeber)
+   .then(data => {
+       res.status(200).send(data);
+   })
+   .catch(err => {
+       res.status(500).send({
+           message: err.message || "Some error occured while creating the clubMember"
+       })
+   });
+};
+
+exports.remove = (req, res) => {
+    const id = req.params.inviteId;
+
+    ClubMember.destroy({
+        where: {id: id}
+    })
+    .then(num => {
+        if(num == 1) {
+            res.status(200).send({
+                message: "ClubMember deleted successfully!"
+            });
+        } else {
+            res.status(404).send({
+                message: `ClubMember with id:${id} was not found.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete ClubMember with id:" + id
+        });
+    });
+}
+
+exports.list = (req, res) => {
+    const clubId = req.params.id;
+
+    ClubMember.findAll({
+        where: {club_id: clubId}
+    })
+    .then(data => {
+        res.status(200).send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving the member list."
+        });
+    });
+}
+
---
+++ app/routes/club-member.routes.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+  const club_members = require("../controllers/club-member.controller.js");
+
+  var router = require("express").Router();
+
+  router.post("/create", club_members.create);
+
+  router.delete("/remove/:inviteId", club_members.remove);
+
+  router.get("/:clubId", club_members.list);
+
+  app.use('/api/club/member', router);
+}
