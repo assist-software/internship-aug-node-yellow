7e4e451cef55b66ba14c80246352c4b721efb866
Andrei-Sebastian <andrei.seby45@yahoo.com>
2020-08-13 14:48:38 +0300

    +

---
+++ app/middlewares/authJwt.js
@@ -0,0 +1,83 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!"
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Admin Role!"
+      });
+      return;
+    });
+  });
+};
+
+isCoach = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "coach") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Coach Role!"
+      });
+    });
+  });
+};
+isAthlete = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+      user.getRoles().then(roles => {
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "athlete") {
+            next();
+            return;
+          }
+        }
+  
+        res.status(403).send({
+          message: "Require Athlete Role!"
+        });
+      });
+    });
+  };
+
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isCoach: isCoach,
+  isAthlete: isAthlete
+};
+module.exports = authJwt;
