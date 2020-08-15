0bdb4664aaee5b8d74c149f4cc4a4bf4c89a4462
Andrei-Sebastian <andrei.seby45@yahoo.com>
2020-08-15 20:35:47 +0300

    add

--- app/routes/event-request.routes.js
+++ app/routes/event-request.routes.js
@@ -1,4 +1,4 @@
-module.exports = (app) => {
+module.exports = app => {
     const event_requests = require("../controllers/event-request.controller.js");
 
     var router = require("express").Router();
