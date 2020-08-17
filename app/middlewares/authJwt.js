a4bd378cb9563c7608adba1ed95429f447df5e23
Denis-Copilu <deniscopilu99@yahoo.com>
2020-08-17 10:19:22 +0300

    repost

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -3,13 +3,13 @@ const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
 let userId ;
-const authJwt = {
-  verifyToken: null,
-  role_id: null
-};
-authJwt.verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
 
+exports.authJwt = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  const authJwt = {
+    verifyToken: null,
+    role_id: null
+  };
   if (!token) {
     return res.status(403).send({
       message: "No token provided!"
@@ -25,14 +25,14 @@ authJwt.verifyToken = (req, res, next) => {
     userId = decoded.id;
     next();
   });
-};
-
-
   User.findByPk(userId).then(user=>{
     authJwt.role_id = user.role_id;
   }
   ).catch(err=>{
     return res.status(500).send({message: err.message });
   });
+return authJwt;
+};
 
-module.exports = authJwt;
+
+  
