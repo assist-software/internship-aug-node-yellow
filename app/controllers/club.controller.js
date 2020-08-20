3b0fd252f5f6a05616fa608ae20fd02572b57523
Madalina <madalina.agheorghiesei@gmail.com>
2020-08-20 10:36:36 +0300

    fixed list club controller

--- app/controllers/club.controller.js
+++ app/controllers/club.controller.js
@@ -162,17 +162,18 @@ exports.list = (req, res) => {
         resClub[i].dataValues["ownerFirstName"] = usersData[i].first_name;
         resClub[i].dataValues["ownerLastName"] = usersData[i].last_name;
       }
-      console.log(resClub);
       return Promise.all(resClub.map(entry => getClubMembers(entry)));
     })
     .then(clubsMembers => {
-      console.log(clubsMembers);
       return Promise.all(clubsMembers.map(entry => findMember(entry)));
     })
     .then(membersData => {
-      console.log(membersData);
       for (let i = 0; i < resClub.length; i++) {
-        resClub[i].dataValues["members"] = membersData[i];
+        if(membersData[i] != null) {
+          resClub[i].dataValues["members"] = membersData[i].dataValues;
+        } else {
+          resClub[i].dataValues["members"] = [];
+        }
       }
       
       res.status(200).send(resClub);
